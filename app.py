775083b09b301413064ecd1a7e9e6c0ee806a620
TimWright512 <35933250+TimWright512@users.noreply.github.com>
2023-07-08 11:55:26 -0500

    updated app EDA csv files

--- app.py
+++ app.py
@@ -12,9 +12,38 @@ if not show_manuf_1k_ads:
     df = df.groupby('manufacturer').filter(lambda x: len(x) > 1000)
 
 st.dataframe(df)
-st.header('Histogram of Vehicle Types by their Condition')
-st.write(px.histogram(df, x='type', color='condition'))
+st.header('Compare price distribution between manufacturers')
+manufac_list = sorted(df['manufacturer'].unique())
 
+manufacturer_1 = st.selectbox(
+    label='Select manufacturer 1',
+    options=manufac_list,
+    index=manufac_list.index('ford')
+)
+
+manufacturer_2 = st.selectbox(
+    label='Select manufacturer 2',
+    options=manufac_list,
+    index=manufac_list.index('honda')
+)
+
+mask_filter = (df['manufacturer'] == manufacturer_1) | (df['manufacturer'] == manufacturer_2)
+df_filtered = df[mask_filter]
+
+normalize = st.checkbox('Normalize histogram', value=True)
+if normalize:
+    histnorm = 'percent'
+else:
+    histnorm = None
+
+fig = px.histogram(df_filtered,
+                   x='price',
+                   nbins=30,
+                   color='manufacturer',
+                   histnorm=histnorm,
+                   barmode='overlay')
+
+st.write(fig)
 
 st.header('Bar Chart of `Manufacturer` and the amount of `Days Listed`')
 
